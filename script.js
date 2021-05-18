646085ddee26b6cc8296b58c6d6b45a90165f059
Oleg55 <Shevchenko.oleg.1992@gmail.com>
2021-05-18 13:51:57 +0300

    add do while

--- script.js
+++ script.js
@@ -10,10 +10,49 @@ const personalMovieDB = {
 	privat: false
 };
 
-const lustOfFilmsFirst = prompt('Один из последних просмотренных фильмов?', 'Титаник'),
-			ratingOfFilmsFirst = +prompt('На сколько оценете его?', '100'),
-			lustOfFilmsSecond = prompt('Один из последних просмотренных фильмов?', 'Титаник2'),
-			ratingOfFilmsSecond = +prompt('На сколько оценете его?', '100');
+let lustOfFilmsFirst, ratingOfFilmsFirst, lustOfFilmsSecond, ratingOfFilmsSecond;
+
+for (let i = 0; i < 2; i++) {
+
+}
+
+do{
+	lustOfFilmsFirst = prompt('Один из последних просмотренных фильмов?', '');
+	if (lustOfFilmsFirst == ''){
+		alert('Введите значение');
+	}
+}
+while(lustOfFilmsFirst == '');
+
+
+do{
+	ratingOfFilmsFirst = +prompt('На сколько оценете его?', '');
+	if (ratingOfFilmsFirst == ''){
+		alert('Введите значение');
+	}
+}
+while(ratingOfFilmsFirst == '');
+
+
+do{
+	lustOfFilmsSecond = prompt('Один из последних просмотренных фильмов?', '');
+	if (lustOfFilmsSecond == ''){
+		alert('Введите значение');
+	}
+}
+while(lustOfFilmsSecond == '');
+
+
+do{
+	ratingOfFilmsSecond = +prompt('На сколько оценете его?', '');
+	if (ratingOfFilmsSecond == ''){
+		alert('Введите значение');
+	}
+}
+while(ratingOfFilmsSecond == '');
+
+
+
 
 personalMovieDB.movies[lustOfFilmsFirst] = ratingOfFilmsFirst;
 personalMovieDB.movies[lustOfFilmsSecond] = ratingOfFilmsSecond;
